ba87edbb34f5cbaa6a0155353bf56112e3003acd
Vineet <vineetgoyat1@gmail.com>
2026-06-26 23:44:40 +0530

    update profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,3 +7,18 @@ exports.getUsers = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+exports.updateProfile = async (req, res) => {
+  try {
+    const { name, avatar } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { name, avatar },
+      { new: true }
+    ).select("-password");
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
