8ff16d4cef3cadd5a028227682a3749edff255d8
Vineet <vineetgoyat1@gmail.com>
2026-06-26 23:44:02 +0530

    get user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,9 @@
 const User = require("../models/User");
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
