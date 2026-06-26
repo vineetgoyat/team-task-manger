1aa7edc03a771a14957ddb931d0676620f238af2
Vineet <vineetgoyat1@gmail.com>
2026-06-26 23:38:28 +0530

    get task function added

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,1 +1,21 @@
 const Task = require("../models/Task");
+
+exports.getTasks = async (req, res) => {
+  try {
+    const filter = req.user.role === "admin" ? {} : { assignedTo: req.user._id };
+    const { status, priority, search } = req.query;
+    if (status) filter.status = status;
+    if (priority) filter.priority = priority;
+    if (search) filter.title = { $regex: search, $options: "i" };
+
+    const tasks = await Task.find(filter)
+      .populate("assignedTo", "name email avatar")
+      .populate("createdBy", "name email")
+      .populate("comments.user", "name avatar")
+      .sort({ createdAt: -1 });
+
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
