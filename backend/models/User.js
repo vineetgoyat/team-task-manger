0bb1f25569d110c12daef5d4620fa9249ae28b1e
Vineet <vineetgoyat1@gmail.com>
2026-06-26 23:25:05 +0530

    user.js now ?

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,3 +19,7 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
