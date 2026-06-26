7ca3c31bac03ce8c40c35c1dbfa12b9dd1c496e9
Vineet <vineetgoyat1@gmail.com>
2026-06-26 23:29:06 +0530

    working.....

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,1 +1,9 @@
 const mongoose = require("mongoose");
+const commentSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    text: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
