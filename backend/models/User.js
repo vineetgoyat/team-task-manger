de338bbb5ca9a96b80bb83d2a8e53ac0902263b2
Vineet <vineetgoyat1@gmail.com>
2026-06-26 23:24:21 +0530

    user.js maybe now

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,3 +13,9 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
